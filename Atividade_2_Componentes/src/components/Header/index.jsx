4d005ad5bedfad722843741ba5a810633c7a5608
Eduardo Alvarez <opticoedu@gmail.com>
2023-11-24 11:02:01 -0300

    ultima revisão ok

--- Atividade_2_Componentes/src/components/Header/index.jsx
+++ Atividade_2_Componentes/src/components/Header/index.jsx
@@ -11,7 +11,7 @@ export function Header() {
             <h1>Transformando <span>espaço</span>, Criando <span>história</span></h1>
 
             <p>Nós criamos ambientes exclusívos e únicos, com muito bom gosto e profissionalismo,
-               investimos tempo e <span>dedicação no seu projeto.</span> Nosso compromisso vai além de simplesmente decorar.
+               investimos tempo e <span>dedicação no seu projeto.</span> Nosso compromisso vai além de simplesmente decorar.<br></br><br></br>
 
 
               Tenha <span>ambientes limpos</span> e bem decorados, transmitindo elegância  e finesse para todo aquele que estiver
